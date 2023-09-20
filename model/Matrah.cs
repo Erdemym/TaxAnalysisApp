94d44dbb3590473a6039ee5f7f2ff3a7e1e2f6a6
Erdemym <erdemym@gmail.com>
2023-09-20 17:06:22 +0300

    modified:   model/Ayar.cs 	modified:   model/Matrah.cs 	modified:   model/SBK.cs 	modified:   model/TabloH.cs 	modified:   .gitignore 	modified:   Program.cs 	modified:   TaxAnalysisApp.csproj 	modified:   model/Ayar.cs 	modified:   model/SBK.cs 	modified:   model/TabloH.cs

--- model/Matrah.cs
+++ model/Matrah.cs
@@ -1,14 +1,14 @@
 public class Matrah
 {
-    public string Kanun { get; set; }
+    public string? Kanun { get; set; }
     public int VergiNo { get; set; }
-    public string Unvan { get; set; }
+    public string? Unvan { get; set; }
     public int Yil { get; set; }
     public int VergiKodu { get; set; }
-    public string VergiAdi { get; set; }
-    public string OdemeBilgisi { get; set; }
-    public string ManuelDuzenleme { get; set; }
-    public string EkAciklama { get; set; }
-    public string IliskiVergiNo { get; set; }
-    public string IliskiUnvan { get; set; }
+    public string? VergiAdi { get; set; }
+    public string? OdemeBilgisi { get; set; }
+    public string? ManuelDuzenleme { get; set; }
+    public string? EkAciklama { get; set; }
+    public string? IliskiVergiNo { get; set; }
+    public string? IliskiUnvan { get; set; }
 }
